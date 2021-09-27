6556d6dba29f2de372924563131f997d5074c2f7
Sebasan07 <68249217+Sebasan07@users.noreply.github.com>
2021-09-27 14:09:03 -0500

    Update Medico.java

--- clinica.entidades/src/main/java/com/venko/ws/rest/clinica/entidades/Medico.java
+++ clinica.entidades/src/main/java/com/venko/ws/rest/clinica/entidades/Medico.java
@@ -2,7 +2,9 @@ package com.venko.ws.rest.clinica.entidades;
 
 public class Medico extends Persona{
 
-	public Medico() {
-		super();
+	public Medico(String primerNombre, String segundoNombre, String primerApellido, String segundoApellido,
+			String tipoDocumento, String numeroDocumento, Date fechaExpedicionDoc) {
+		super(primerNombre, segundoNombre, primerApellido, segundoApellido,
+				tipoDocumento, numeroDocumento,fechaExpedicionDoc);
 	}
 }
